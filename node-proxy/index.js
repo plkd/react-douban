115eb95afa2ec194be392dfe45b100785daeddb7
lijiliang <ljlprismo@gmail.com>
2019-03-01 14:42:59 +0800

    update

---
+++ node-proxy/index.js
@@ -0,0 +1,9 @@
+/**
+ * @date:2019-03-01$ 14:40$
+ * @author:ljl$
+ */
+let express = require('express')
+let request = require('superagent')
+
+let HOST = 'http://api.douban.com/v2'
+app.set('port', (process.env.PORT || 8081))
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './test.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App ">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -17,7 +18,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React1  <span className="test">jjjjjj</span>
           </a>
         </header>
       </div>
